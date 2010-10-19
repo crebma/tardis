924a9be6e2632a4eba1b387213d800a3639078a6
mudetroit <mlaforest@pillartechnology.com>
2010-10-19 19:06:08 -0400

    Initialize the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tardis::Application.config.secret_token = '5e603e9bb158222a3eb528b607dc0df4af6f7f2ac5683259e9eb643567ff1b35d7fe78f8fab4c3511a16924b5133214f2f1c976aa69123a74fd0c8780568240f'
