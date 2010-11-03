9ba3761f9038cc9337761c25b5c73c77cbeb9ad8
magnus stahre <ms@xy.org>
2010-11-02 21:10:45 -0400

    magnus: default to user view

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Tardis::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "timesheets#index"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
